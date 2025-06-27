b75af7d2848ab3f74583801ffde1f2b26953859d
Trey G <dove-ascent-0q@icloud.com>
2025-06-27 14:38:37 -0400

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Trey Gaines on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CurrencyConverterTests/CurrencyConverterTests.swift
@@ -0,0 +1,16 @@
+//
+//  CurrencyConverterTests.swift
+//  CurrencyConverterTests
+//
+//  Created by Trey Gaines on 6/27/25.
+//
+
+import Testing
+
+struct CurrencyConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
